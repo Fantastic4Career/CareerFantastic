a900c99da6601b75527fa658fdfde8d62abbaa07
Shengtao Li <sevenlist0110@gmail.com>
2017-03-18 13:18:49 -0700

    [update] add firebase configs

---
+++ assets/javascript/firebaseLogic.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA1g2zHte-oWy_n2r_BQCGZ7ydZnAaEK3k",
+  authDomain: "careerfantastic-e91d7.firebaseapp.com",
+  databaseURL: "https://careerfantastic-e91d7.firebaseio.com",
+  storageBucket: "careerfantastic-e91d7.appspot.com",
+  messagingSenderId: "389453357831"
+};
+firebase.initializeApp(config);
