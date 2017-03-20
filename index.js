2af686777261d3d7e18fa066b0376e03bdf414c9
Shengtao Li <sevenlist0110@gmail.com>
2017-03-19 22:47:49 -0700

    use correct port on heroku

--- index.js
+++ index.js
@@ -49,6 +49,6 @@ app.get('/*', function(req, res){
   res.redirect('/');
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 });
