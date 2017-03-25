3383293c7a10d906d3170e4650102b0057698c27
Eleanor Mark <emark@Eleanors-MBP.attlocal.net>
2017-03-24 17:48:31 -0700

    retrieve lat/long from firebase

--- assets/js/firebaseInit.js
+++ assets/js/firebaseInit.js
@@ -8,3 +8,25 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 var jobsAppliedDB = database.ref("/test");
+var appliedMarkers = [];
+
+$( document ).ready(function() {
+	 // Handler for .ready() called.
+	 database.ref("/test").on("value", function(snapshot){
+	     // storing the snapshot.val() in a variable for convenience
+	  	 var sv = snapshot.val();
+	  	 // Getting an array of each key In the snapshot object
+	  	 var svArr = Object.keys(sv);
+
+	  	svArr.forEach(function (index) {
+
+	    var marker = [];
+	    marker.push(sv[svArr[index]].company);
+	    marker.push(sv[svArr[index]].latitude);
+	    marker.push(sv[svArr[index]].longitude);
+	    appliedMarkers.push(marker);
+		});
+		console.log("appliedMarkers>>>" + appliedMarkers[3]);
+	})
+});
+
