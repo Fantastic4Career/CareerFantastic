605be180f5ad2b9bab83c7b4f4cad2bd0eb1e319
Shengtao Li <sevenlist0110@gmail.com>
2017-03-24 19:01:05 -0700

    [update] displaying applied jobs from firebase

--- assets/js/appliedJobs.js
+++ assets/js/appliedJobs.js
@@ -4,8 +4,22 @@ var appliedJobsVue = new Vue({
     jobs: jobsAppliedDB.limitToLast(25)
   },
   methods: {
-    noop: function () {
-      
+    removeJob: function (job) {
+      // remove current job
+      var index = this.jobs.indexOf(job);
+      var dbIndex = job[".key"];
+      this.jobs.splice(index, 1);
+
+      jobsAppliedDB.child(dbIndex).remove();
+      //
+      // put it into jobApplied array
+    }, 
+    updateJob: function (job) {
+      console.log("job is >>>>", job);
+      var dbIndex = job[".key"];
+      console.log("dbIndex is>>>", dbIndex);
+      console.log(job.location);
+      jobsAppliedDB.child(dbIndex).update(_.omit(job, ".key"));
     }
   }
 })
